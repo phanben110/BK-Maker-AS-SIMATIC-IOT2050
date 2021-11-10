d3db10bd2caa89e522568ce61cc9341afce4795f
Quynguyen1603 <quyms10@gmail.com>
2021-11-10 10:13:36 +0700

    new main file

---
+++ Application/main.py
@@ -0,0 +1,11 @@
+from PyQt5.QtWidgets import QMainWindow,QApplication
+from ui_v2 import *
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
