d0cb998d1e4e9394e8ca2cc83b21223fae1e3f8f
Ubuntu <ubuntu@ip-172-31-23-152.ec2.internal>
2021-11-15 07:46:17 +0000

    update file simaticIot2050.py update stream data

--- simaticIot2050.py
+++ simaticIot2050.py
@@ -1,7 +1,7 @@
 #Import library custom for Tuining Machine Learning
 from linearRegession.BEN_TuningPID import TuningPID as TuningPIDML 
 #Import libray custom for send and receive data with AWS 
-from aws.BEN_DynamoDB import DynamoDB as cloudAWS 
+from AWS.BEN_DynamoDB import DynamoDB as cloudAWS 
 
 import time  
 from numpy import random 
@@ -22,13 +22,22 @@ cloud = cloudAWS()
 
 #----------Begin interact with cloudAWS--------------
 # receive data from cloud aws
+print ( "*****BK-MAKER AS TEAM*****" ) 
+print ( "*****Set up*****" ) 
 dataDevice =  cloud.receiveData(table="Device",id=1)
-dataDevice =  cloud.receiveData(table="ML",id=1)
-print (dataDevice)
+dataML =  cloud.receiveData(table="ML",id=1)
+idDevice = int(dataDevice["currentID"])
+setpoint = int(dataML["control"]["setpoint"])
+
+print ( f"current ID: {idDevice}" ) 
+print ( f"current setpoint: {setpoint}" ) 
+#dataDevice =  cloud.receiveData(table="ML",id=1)
+#print (dataDevice)
 
 # get the final ID in the cloud 
-finalID = cloud.getFinalID(table="Device") 
-print (finalID)
+#finalID = cloud.getFinalID(table="Device") 
+
+#print (finalID)
 
 #send data to table Machine Learning 
 #cloud.sendData(table="MachineLearning",id=26,name="Ben Dep Trai",online=True, kp=3.333,ki=4,kd=5, setPoint=34, controlBit=2)
@@ -61,20 +70,32 @@ print (finalID)
 #               k1=1, k2=2, k3=3
 #               )
 #
-Kp = 0.03999999910593033
-Kd = 0.03999999910593033
-Ki = 0.001500000013038516
-count = 0
+Kp = 0.0342504
+Kd = 0.0377881
+Ki = 0.0094469
+count = idDevice + 1 
 check=False
+am = 1
+#setpoint = 150
+time.sleep(2) 
 while True: 
-    setpoint = randint(100,250)
-    x=random.rand(3)/2
-    Kp = Kp + Kp*x[0]
-    Ki = Ki + Ki*x[1]
-    Kd = Kd + Ki*x[2]
-    print (f"Current ID: {count}, Kp = {Kp}, Ki = {Ki}, Kd = {Kd}, setpoint: {setpoint}")
+    if setpoint == 150:
+        setpoint = 200
+    else :
+        setpoint = 150
+    #setpoint = randint(100,250)
+    timeBegin = time.time()
+    x=random.rand(3)/4
+    Kp = Kp + Kp*x[0]*am
+    Ki = Ki + Ki*x[1]*am 
+    Kd = Kd + Ki*x[2]*am
+    print (f"*****step 1: Current ID: {count}, Kp = {Kp}, Ki = {Ki}, Kd = {Kd}, setpoint: {setpoint}")
     
-    settlingTime =  cloud.receiveData(table="Device",id=1)["quality"]["settlingTime"]
+
+    currentData = cloud.receiveData(table="Device",id=1)
+    settlingTime =  currentData["quality"]["settlingTime"]
+    overshoot = currentData["quality"]["overshoot"]
+
     
     time.sleep(1)
     
@@ -84,7 +105,7 @@ while True:
                    kp=Kp,ki=Ki,kd=Kd,
                    movePara=True,moveToPos=False,stop=False, autoTune=False,
                    setpoint=setpoint)
-    print ("---------------send step 1---------------")
+    print ("*****step 2: set True movePara")
     time.sleep(2)
 
     cloud.sendData(table="ML",
@@ -109,22 +130,26 @@ while True:
    #                kp=Kp,ki=Ki,kd=Kd,
    #                movePara=False,moveToPos=False,stop=False, autoTune=False,
    #                setpoint=setpoint)
-    print ("---------------send step 2---------------")
+    print ("*****step 3: set False")
     time.sleep(2)
-    if setpoint >= 266 : 
-        setpoint = 126
 
-    
+    checkPrint = True  
     while check == False: 
         dataDevice =  cloud.receiveData(table="Device",id=1)
         
         idDevice = int(dataDevice["currentID"]) 
         settlingDevice = dataDevice["quality"]["settlingTime"]
-        print (f"---------------check currentID=={count} and settling time != {settlingTime} step 3---------------")
-        print ( idDevice, settlingDevice )
-        if  settlingDevice != settlingTime : 
+        overshootDevice = dataDevice["quality"]["overshoot"] 
+        if checkPrint == True:
+            print (f"*****step 4: check currentID=={count} and settlingTime != {settlingTime} and overshoot != {overshoot}")
+            print ( f"cloud: currentID: {idDevice}, settlingTime: {settlingDevice}, overshoot: {overshootDevice}" )
+            checkPrint = False
+        print (".", end="", flush=True)
+        time.sleep(0.1)
+        if  (idDevice == count) and (settlingDevice != settlingTime) and (overshoot != overshootDevice): 
             check == True 
-            print ("---------------step 3 is OK---------------")
+            print("")
+            print ("*****step 5: Prepare for new ID")
             time.sleep(1)
 
             cloud.sendData(table="ML",
@@ -134,5 +159,7 @@ while True:
                            movePara=False,moveToPos=False,stop=False, autoTune=False,
                            setpoint=setpoint)
             count +=1 
+            am = am*(-1)
             break 
-    print ( f"Done {count}" )
+    print ( f"-----Done {count} , runing time :{(time.time() - timeBegin)} s-----" )
+    print ("")
